fc18008a679866e83463ed8cd26e9bc645f96394
Emilia Carvajal <emilia.carvajalm@gmail.com>
2022-12-24 22:30:20 -0400

    Adding code for major number and number of repetitions before zero

---
+++ MajorNumberNumberRepetitions/MajorNumbNumberRepetitions/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program {
+  public static void Main (string[] args) 
+  {
+    Console.WriteLine ("Por favor ingrese el numero:");
+    string MayorString= Console.ReadLine();
+    int MayorInt = Int32.Parse(MayorString);
+    int cantidad=1;
+    int NumeroInt=0;
+    do
+    {
+      Console.WriteLine ("Por favor ingrese otro numero:");
+      string NumeroString= Console.ReadLine();
+      NumeroInt = Int32.Parse(NumeroString);
+      if (NumeroInt > MayorInt)
+      {
+        MayorInt=NumeroInt;
+        cantidad=1;        
+      }
+      else if (NumeroInt==MayorInt)
+      {       
+        cantidad=cantidad+1;        
+      } 
+    }while (NumeroInt!=0);
+      
+    Console.WriteLine ("El numero mayor es:" +MayorInt+ " con repeticiones:" +cantidad);
+  }
+}
